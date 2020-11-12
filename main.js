de65ea7e44a80f1142782cdc3d6d737b5d6c2f8b
Linda Söder <lindasoder@Lindas-MacBook-Pro.local>
2020-11-12 23:44:41 +0100

    add js change h1 text

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 function mouseDown(color) {
     color.style.backgroundColor = "#D90368";
 }
+
+function h1Change(id) {
+    id.innerHTML = "Designers.", 
+    id.innerHTML = "Developers.", 
+    id.innerHTML = "Gamers", 
+    id.innerHTML = "Hello Everyone!";
+    
+}

0f1c22c4edbfc67f035bdc2f3d84d8b7a9b0d274
Linda Söder <lindasoder@Lindas-MacBook-Pro.local>
2020-11-13 12:14:00 +0100

    add script for h1 heading and script for hamburger menu

--- main.js
+++ main.js
@@ -14,10 +14,43 @@ function mouseDown(color) {
     color.style.backgroundColor = "#D90368";
 }
 
-function h1Change(id) {
-    id.innerHTML = "Designers.", 
-    id.innerHTML = "Developers.", 
-    id.innerHTML = "Gamers", 
-    id.innerHTML = "Hello Everyone!";
+function h1Change() {
+    let h1 = document.getElementById('h1');
     
+    if (h1.innerHTML = "Hello world.") {
+        h1.innerHTML = "Designers."; 
+    } else if (h1.innerHTML = "Designers.") {
+        h1.innerHTML = "Developers.";
+    } else if (h1.innerHTML = "Developers.") {
+        h1.innerHTML = "Gamers.";
+    } else if 
+        (h1.innerHTML = "Gamers.") {
+        h1.innerHTML = "Hello Everyone!";
+    } else if (h1.innerHTML = "Hello Everyone!") {
+        h1.innerHTML = "Hello world.";
+    }
+}
+
+/**
+ * Hides background on hamburger menu when link is clicked.
+ */
+
+let home1 = document.getElementById('home1');
+home1.onclick = function() {
+    header.style.height = null;
+}
+
+let about1 = document.getElementById('about1');
+about1.onclick = function() {
+    header.style.height = null;
+}
+
+let skills1 = document.getElementById('skills1');
+skills1.onclick = function() {
+    header.style.height = null;
+}
+
+let contact1 = document.getElementById('contact1');
+contact1.onclick = function() {
+    header.style.height = null;
 }
